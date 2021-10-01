eab860c099957688d4dce9cff512c32de82ca215
BergeDios <santiago.goyret@gmail.com>
2021-10-01 11:15:20 -0700

    task 4.1 if else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar(10);
+	return (0);
+}
