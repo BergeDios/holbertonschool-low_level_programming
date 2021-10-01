1e71b68c4bd6447ec6ce363b663eae2e7cdf35dc
BergeDios <santiago.goyret@gmail.com>
2021-10-01 11:35:48 -0700

    task 6.1 if else

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
