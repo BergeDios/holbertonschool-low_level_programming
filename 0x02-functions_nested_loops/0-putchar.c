a1411cd4be21880aa1fcebfa2c718c10a6f82958
BergeDios <santiago.goyret@gmail.com>
2021-10-05 06:57:45 -0700

    task 0 functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - write a script that prints _puthcar followed by a line
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
