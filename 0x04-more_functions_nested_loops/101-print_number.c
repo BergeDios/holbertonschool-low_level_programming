2cd8d2edf2981884022ca0019d4353bf19fce50a
BergeDios <santiago.goyret@gmail.com>
2021-10-07 11:15:25 -0700

    task 12 adding /10 and %10

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,16 @@
 
 void print_number(int n)
 {
+	unsigned int positive = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		positive = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (positive / 10)
+		print_number(positive / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(positive % 10 + '0');
 }
