304e85a81f6d1f89d736f301e8884c1bd8b4b5d1
BergeDios <santiago.goyret@gmail.com>
2021-10-12 14:16:26 -0700

    task 5 con while para definir el largo del string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *@s: string
+ *Return: nothing
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	int c, b = 0;
+
+	while (s[a] != 0)
+		a++;
+	a--;
+	while (a > b)
+	{
+		 c = s[a];
+		s[a--] = s[b];
+		s[b++] = c;
+	}
+}
