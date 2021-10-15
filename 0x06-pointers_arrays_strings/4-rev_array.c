ee1aa123d8cb3b69c48ca075096e361e5c882482
BergeDios <santiago.goyret@gmail.com>
2021-10-15 07:03:58 -0700

    task ntil 6 done wait for checker

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array
+ * of integers.
+ * @a: array.
+ * @n: number of elements of the array.
+ * Return: no return.
+ */
+
+void reverse_array(int *a, int n)
+{
+
+	int tmp, beg = 0;
+	int end = n - 1;
+
+	while (beg < end)
+	{
+		tmp = *(a + beg);
+		*(a + beg) = *(a + end);
+		*(a + end) = tmp;
+		beg++, end--;
+	}
+}
