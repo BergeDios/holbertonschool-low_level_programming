1e9f4fb5efec1003743b8a62fcc58b95a6e5c4ac
BergeDios <santiago.goyret@gmail.com>
2021-10-18 10:39:56 -0700

    task 2 if outside for with returns

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,7 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; *(s + i) != '\0' && *(s + i) == c; i++)
+	for (; *(s + i) != '\0' && *(s + i) != c; i++)
 		;
 
 	if (*(s + i) == c)
