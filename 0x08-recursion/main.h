d0fe6156810c18bc7f4e0832b711f6dd02aa4ab8
BergeDios <santiago.goyret@gmail.com>
2021-10-20 13:24:50 -0700

    correct if condition to return 0 numbers that have another divisor

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int rootfinder(int n, int root);
 int _sqrt_recursion(int n);
+int findprime(int n, int d);
 int is_prime_number(int n);
 int _putchar(char);
