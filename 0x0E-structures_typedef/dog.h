4e248c37b083adb65c07f520d1347e0b3cad44bc
BergeDios <santiago.goyret@gmail.com>
2021-11-01 09:45:52 -0700

    new typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/*
+ * dog_t - new name for type struct dog
+ */
+typedef struct dog dog_t;
+
 #endif
