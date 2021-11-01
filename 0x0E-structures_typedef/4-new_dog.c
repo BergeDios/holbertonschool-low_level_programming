bc9fac282f6fceb5e4f6aca966578844ebe99db3
BergeDios <santiago.goyret@gmail.com>
2021-11-01 10:25:15 -0700

    task 4 new dog with strdup

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - return a pointer to new space with copied string
+ * @str: string to be copied
+ * Return: pointer to duplicated string or null if there's no memory
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0, j = 0;
+	char *da;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (; str[i] != 0; i++)
+		;
+	da = malloc(sizeof(char) * i + 1);
+	if (da == NULL)
+	{
+		return (NULL);
+	}
+	while (j <= i)
+	{
+		da[j] = str[j];
+		j++;
+	}
+	return (da);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: owner of new dog
+ * Return: new dog or null
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog2;
+
+	dog2 = malloc(sizeof(dog_t));
+	if (dog2 == NULL)
+		return (NULL);
+	dog2->name = _strdup(name);
+	dog2->age = age;
+	dog2->owner = _strdup(owner);
+	return (dog2);
+}
