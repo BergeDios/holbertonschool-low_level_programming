1dd293b8fe26e7b004e3ccb8f044d8bb1ef163cd
BergeDios <santiago.goyret@gmail.com>
2021-11-04 14:01:11 -0700

    task 3 first commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,23 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+/**
+ * struct all - struct all
+ *
+ *@char: letter for possible formats
+ *@f: function associated
+ */
+typedef struct all
+{
+	char *lf;
+	void (*f)(va_list ap);
+}datatype;
+
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
