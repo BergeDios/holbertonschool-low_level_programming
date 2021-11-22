0018369c0c3aecd517a4770393d71e97cbb6f2e6
BergeDios <santiago.goyret@gmail.com>
2021-11-22 12:05:21 -0800

    task 2 working succesfully

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,18 +14,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
-	}
-	while (text_content[lt])
-		lt++;
-	nw = write(fd, text_content, lt);
-	if (nw == -1)
-	{
-		close(fd);
-		return (-1);
+		while (text_content[lt])
+			lt++;
+		nw = write(fd, text_content, lt);
+		if (nw == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
