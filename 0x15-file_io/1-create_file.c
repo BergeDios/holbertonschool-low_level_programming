8b5e83d48116cd1c042797c29d0d4cc0f2a8c558
BergeDios <santiago.goyret@gmail.com>
2021-11-22 08:46:59 -0800

    fix while loop in task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,9 +21,8 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	while (text_content)
+	while (text_content[lt])
 		lt++;
-	lt--;
 	nw = write(fd, text_content, lt);
 	if (nw == -1)
 	{
